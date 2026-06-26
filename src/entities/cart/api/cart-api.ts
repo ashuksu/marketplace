73c101e4aa4c73db0ddd6760ce86ba0cf552cb83
ashuksu <ashuksu@gmail.com>
2026-06-26 22:52:10 +0200

    feat: refactor cart API and remove cart slice for improved structure and clarity

--- src/entities/cart/api/cart-api.ts
+++ src/entities/cart/api/cart-api.ts
@@ -1,9 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-type ServerCartItem = {
+type CartItem = {
   productId: string;
   title: string;
   price: number;
+  quantity: number;
+};
+
+type AddToCartRequest = {
+  productId: string;
   quantity: number;
 };
 
@@ -17,13 +22,13 @@ export const cartApi = createApi({
   tagTypes: ['Cart'],
 
   endpoints: (builder) => ({
-    getCart: builder.query<ServerCartItem[], void>({
+    getCart: builder.query<CartItem[], void>({
       query: () => '/cart',
 
       providesTags: ['Cart'],
     }),
 
-    addToCart: builder.mutation<ServerCartItem[], { productId: string; quantity: number }>({
+    addToCart: builder.mutation<CartItem[], AddToCartRequest>({
       query: (body) => ({
         url: '/cart',
         method: 'POST',
